cd547841633cf6eb30db21c9edca006a7bc3e21f
dfmoscoso23 <83509181+dfmoscoso23@users.noreply.github.com>
2021-11-26 22:09:11 +0000

    This bot explains Data Science Codes Added 2 Cog classes for listeners: One for Data Science learning and the oder for party mode. Both can be turn off with the command "duerme"

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from datetime import datetime
 import random
 import pandas as pd
 import nltk
-from tokenizador_frases import tokenizar # asi!!Exacto!:D jaja
-#from modes import profe_mode
+from tokenizador_frases import tokenizar 
 
 df_data=pd.io.json.read_json('datacheat.json')
 df_data=df_data.T
@@ -25,55 +24,62 @@ df=pd.io.json.read_json('frasest.json')
 df_usuarios=pd.read_csv('pedidos.csv',delimiter=";")
 
 
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="_help"))
+    print('My bot is ready')
+
 @client.command()
 async def despierta(ctx):
-  await ctx.send('Chicos vamos serios, hablen')
-  @client.event
-  async def on_message(message):
-    if message.author != client.user:
-      if "duerme" in message.content:
-        await client.logout()
-      if message.content in list(df_data.key):
-        await message.channel.send(str(df_data[df_data['key']==str(message.content)]["value"]))
-
-  @client.command() 
-  async def party(ctx):
+    client.add_cog(Modoserio(client))
+    await ctx.send('Chicos vamos serio, hablen')
+@client.command()
+async def party(ctx):
+    client.add_cog(Partymode(client))
     await ctx.send('Chicos estoy aquí, hablen')
-  # Para apacar el bot por completo:
-  #@client.command() 
-  # async def logout(ctx):
-  #   await ctx.send('Shutting down')
-  #   await client.logout() ova con BOT, porrque lo llamamos client
-    @client.event
-    async def on_message(message):
-      if message.author != client.user:
-        if "duerme" in message.content:
-          await client.logout()
-        tokens=nltk.word_tokenize(message.content,"spanish")
-        tokens_limpios=[] 
-        tokens = [token.lower() for token in tokens]
-        for token in tokens: 
-            if token not in palabras_funcionales: 
-                tokens_limpios.append(token)
+@client.command()
+async def duerme(ctx):
+    client.remove_cog('Modoserio')
+    client.remove_cog('Partymode')
+    await ctx.send('Chao, Nos vemos mañana')
 
-        print(tokens_limpios)
+class Modoserio(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self._last_member = None
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author != client.user:
+            print(list(df_data.key))
+            print(message)
+            if message.content in list(df_data.key):
+                await message.channel.send(str(df_data[df_data['key']==str(message.content)]["value"].values))
+class Partymode(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self._last_member = None
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author != client.user:
+            tokens=nltk.word_tokenize(message.content,"spanish")
+            tokens_limpios=[] 
+            tokens = [token.lower() for token in tokens]
+            for token in tokens: 
+                if token not in palabras_funcionales: 
+                    tokens_limpios.append(token)
+                    print(tokens_limpios)
         a=0
-
         if 'hola' in tokens_limpios:
-            await message.channel.send('Hola, ¿Quieres un hack de vida?')
-            a=1
-            # def check(reaction, user):
-            #     return user == message.author and (reaction == 'si' or reaction == 'sí')
-            #try:
-            def check(m):
-              return m.content == 'si'
-
-            try:
-              msg = await client.wait_for('message', check=check)
-              r=random.randint(0,len(df)-1)
-              await message.channel.send(str(df.iloc[r][0]))
-            except:
-              await message.channel.send('Chicos, No os escucho! ¿Queréis o no?')
+          await message.channel.send('Hola, ¿Quieres un hack de vida?')
+          a=1
+          def check(m):
+            return m.content == 'si'
+          try:
+            msg = await client.wait_for('message', check=check)
+            r=random.randint(0,len(df)-1)
+            await message.channel.send(str(df.iloc[r][0]))
+          except:
+            await message.channel.send('Chicos, No os escucho! ¿Queréis o no?')
         elif 'elmo' in tokens_limpios:
           await message.channel.send('Es el verdadero Dios')
           a=1
@@ -93,7 +99,6 @@ async def despierta(ctx):
         elif "coca" in tokens_limpios:
           await message.channel.send('¿Hay que reducir el consumo? Puede ser. Pero también hay que reducir el paro juvenil, el consumo de procesados y la polarización radical...')
           a=1
-
         if a==0:
           for token in tokens_limpios:
             try:
@@ -105,30 +110,14 @@ async def despierta(ctx):
               break
             except:
               pass
-
         df_usuarios.at[len(df_usuarios)]=[message.author,tokens_limpios,datetime.now()]
         df_usuarios.to_csv("pedidos.csv",sep=";",index=False,encoding='utf-8-sig')
 
-# Modo profesor: 
-# @client.command()     
-# async def profe(ctx):
-#   await ctx.send('Chicos vamos serios, hablen')
-#   @client.event
-#   async def on_message(message):
-#     if message.author != client.user:
-#       if "duerme" in message.content:
-#         await client.logout()
-#       if message.content in list(df_data.key):
-#         await message.channel.send(str(df_data[df_data['key']==str(message.content)]["value"]))
+
 
 # Refrescar frases party:
 @client.command()
 async def tokeniza(ctx):
-  tokenizar()
-
-@client.event
-async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="_help"))
-    print('My bot is ready')
+    await tokenizar()
 
 client.run('OTEzMTkxMTk4MTE2Njg3OTIy.YZ65kw.id9oVV8sMjXLkuEpR1FlC6-NNZA')
