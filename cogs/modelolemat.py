8b18cc2d78007314c6e83bf9947f5cdbb6c0f7c3
David <dfmoscoso23@gmail.com>
2021-12-09 17:26:50 +0100

    Adding more functions to modelolemat: creación de pool de frases and seleccion de respuesta now are part of modelolemat.

--- cogs/modelolemat.py
+++ cogs/modelolemat.py
@@ -17,9 +17,9 @@ nltk.download('vader_lexicon')
 nltk.download('stopwords')
 palabras_funcionales=nltk.corpus.stopwords.words("spanish")
 palabras_funcionales.extend([".", ",", ":", ";", "!", "?","'","jaja","jaj","jajaj","ja","jajaja","jajajajaj","jajaja" ])
-df=pd.io.json.read_json(f'cogs/datos/frasest.json')
-tokens_frases=df.columns.drop(['frase','tokenizado'])
-df_usuarios=pd.read_csv(f'cogs/datos/pedidos.csv',delimiter=";")
+# df=pd.io.json.read_json(f'cogs/datos/frasest.json')
+# tokens_frases=df.columns.drop(['frase','tokenizado'])
+#df_usuarios=pd.read_csv(f'cogs/datos/pedidos.csv',delimiter=";")
 
 def tokenizar(message):
 	tokens=nltk.word_tokenize(message.content,"spanish")
@@ -30,3 +30,28 @@ def tokenizar(message):
 			tokens_limpios.append(token)
 		print(tokens_limpios)
 		return tokens_limpios
+def guardadoinputs(message,tokens_limpios):
+	df_usuarios=pd.read_csv(f'cogs/datos/pedidos.csv',delimiter=";")
+	sep=";.;..;.;;"
+	df_usuarios.at[len(df_usuarios)]=[message.author,tokens_limpios,datetime.now()]
+	df_usuarios.to_csv(f"cogs/datos/pedidos.csv",sep=";",index=False,encoding='utf-8-sig')
+	with open(f"cogs/datos/inputs.csv","a") as fh:
+		fh.write("\n"+str(datetime.now())+sep+str(message.author)+sep+"'"+str(message.content)+"'"+sep+str(tokens_limpios))
+def seleccionrespuesta(frase_pool_pool):
+	r1=random.randint(0,len(frase_pool_pool)-1)
+	frase_pool=frase_pool_pool[r1]
+	r2=random.randint(0,len(frase_pool)-1)
+	print(frase_pool)
+	print(r2)
+	return str(frase_pool.iloc[r2][0])
+
+def creacionpool(tokens_limpios):
+	df=pd.io.json.read_json(f'cogs/datos/frasest.json')
+	tokens_frases=df.columns.drop(['frase','tokenizado'])
+	frase_pool_pool=[]
+	for tok_mes in tokens_limpios:
+		le=math.ceil(len(tok_mes)*(2/3))
+		for tok_fra in tokens_frases:
+			if tok_fra.startswith(tok_mes[:le]):
+				frase_pool_pool.append(df[df[tok_fra]>0])
+	return frase_pool_pool
--- cogs/modoparty.py
+++ cogs/modoparty.py
@@ -20,12 +20,12 @@ from cogs import modelopuntuacion
 # palabras_funcionales=nltk.corpus.stopwords.words("spanish")
 # palabras_funcionales.extend([".", ",", ":", ";", "!", "?","'","jaja","jaj","jajaj","ja","jajaja","jajajajaj","jajaja" ])
 
-df=pd.io.json.read_json(f'cogs/datos/frasest.json')
-tokens_frases=df.columns.drop(['frase','tokenizado'])
+#df=pd.io.json.read_json(f'cogs/datos/frasest.json')
+#tokens_frases=df.columns.drop(['frase','tokenizado'])
 
-df_usuarios=pd.read_csv(f'cogs/datos/pedidos.csv',delimiter=";")
+#df_usuarios=pd.read_csv(f'cogs/datos/pedidos.csv',delimiter=";")
 
-sep=";.;..;.;;"
+#sep=";.;..;.;;"
 
 commands_lista=[
     'Alfobot despierta',
@@ -35,7 +35,7 @@ commands_lista=[
     'Alfobot tokeniza']
 
 #df_recepcion=pd.read_csv("recep.csv",delimiter=";")
-df_recepcion=pd.io.json.read_json(f'cogs/datos/recep.json')
+#df_recepcion=pd.io.json.read_json(f'cogs/datos/recep.json')
 #Modo party
 class Partymode(commands.Cog):
     def __init__(self, client):
@@ -45,13 +45,6 @@ class Partymode(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if (message.author != self.client.user) and (message.content not in commands_lista):
-            # tokens=nltk.word_tokenize(message.content,"spanish")
-            # tokens_limpios=[] 
-            # tokens = [token.lower() for token in tokens]
-            # for token in tokens: 
-            #     if token not in palabras_funcionales: 
-            #         tokens_limpios.append(token)
-            # print(tokens_limpios)
             tokens_limpios=modelolemat.tokenizar(message)
             a=0
             if 'hola' in tokens_limpios:
@@ -96,27 +89,13 @@ class Partymode(commands.Cog):
               await message.channel.send(f'El bitcoin está ahora a {bitcoin_value}€. A qué estás esperando? ')
               a=1
             if a==0:
-              frase_pool_pool=[]
-              for tok_mes in tokens_limpios:
-                le=math.ceil(len(tok_mes)*(2/3))
-                for tok_fra in tokens_frases:
-                  if tok_fra.startswith(tok_mes[:le]):
-                  #if tok_mes[:le] in tok_fra:
-                    frase_pool_pool.append(df[df[tok_fra]>0])
+              frase_pool_pool = modelolemat.creacionpool(tokens_limpios)
               try:
-                r1=random.randint(0,len(frase_pool_pool)-1)
-                frase_pool=frase_pool_pool[r1]
-                r2=random.randint(0,len(frase_pool)-1)
-                print(frase_pool)
-                print(r2)
-                await message.channel.send(str(frase_pool.iloc[r2][0]))
+                await message.channel.send(modelolemat.seleccionrespuesta(frase_pool_pool))
                 a=1
               except:
                 pass
-            df_usuarios.at[len(df_usuarios)]=[message.author,tokens_limpios,datetime.now()]
-            df_usuarios.to_csv(f"cogs/datos/pedidos.csv",sep=";",index=False,encoding='utf-8-sig')
-            with open(f"cogs/datos/inputs.csv","a") as fh:
-                fh.write("\n"+str(datetime.now())+sep+str(message.author)+sep+"'"+str(message.content)+"'"+sep+str(tokens_limpios))
+            modelolemat.guardadoinputs(message,tokens_limpios)
     #Guardado de reacciones a las propias reacciones
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
@@ -125,34 +104,5 @@ class Partymode(commands.Cog):
         user = await self.client.fetch_user(payload.user_id)
         emoji = payload.emoji
         modelopuntuacion.guardarreacciones(self.client.user, message,emoji)
-        # if self.client.user == message.author:
-        #     num_frase=df[df["frase"]==message.content].index.values[0]
-        #     if num_frase in df_recepcion['Frases'].values:
-        #         print(df_recepcion[df_recepcion["Frases"]==num_frase].index.values)
-        #         linea=df_recepcion[df_recepcion["Frases"]==num_frase].index.values
-        #         conjunto=df_recepcion.columns
-        #         if emoji.name in conjunto:
-        #             df_recepcion[emoji.name].iat[linea[0]]+=1
-        #             print("encontrado")
-        #         else:
-        #             print("no encontrado")
-        #             df_recepcion[emoji.name]=0
-        #             df_recepcion[emoji.name].iat[linea[0]]=1
-        #     else:
-        #         u_linea=len(df_recepcion)
-        #         linea_ceros=[0 for x in range(len(df_recepcion.columns))]
-        #         linea_ceros[0]=num_frase
-        #         df_recepcion.at[u_linea]=linea_ceros
-        #         conjunto=set(df_recepcion.columns)
-        #         if emoji.name in conjunto:
-        #             df_recepcion[emoji.name].iat[u_linea]+=1
-        #             print("encontrado-primeravez")
-        #         else:
-        #             print("no encontradoprimeravez")
-        #             #conjunto.add(emoji)
-        #             df_recepcion[emoji.name]=0
-        #             df_recepcion[emoji.name].iat[u_linea]=1
-        # df_recepcion.to_csv(f"cogs/datos/recep.csv",index=False, sep=";")
-        # df_recepcion.to_json(f"cogs/datos/recep.json")
 def setup(client):
     client.add_cog(Partymode(client))
