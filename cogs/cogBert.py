eef0262b73282d3129b84a2fafe3df7e4f5c0e69
TGZ7 <togonlopez@gmail.com>
2021-12-20 16:33:09 +0100

    funElectorCode

--- cogs/cogBert.py
+++ cogs/cogBert.py
@@ -25,6 +25,7 @@ class CogBert(commands.Cog):
         self._last_member = None
     @commands.Cog.listener()
     async def on_message(self, message):
+        client=self.client
         try:
             with open(f'funciones/cogactivo.txt',"r") as ca:
                 cog_activo=ca.read()
@@ -55,32 +56,6 @@ class CogBert(commands.Cog):
                     await message.channel.send(str("TOMAS AQUI VA TU MAGIA"))
                     client=self.client
                     await funElectorCode.electorCode(lista_tokens,message,client)
-                    #self.client.load_extension(f'cogs.cogElectorCode')
-                    # codigo=[]
-                    # for i,trigger in enumerate(ml_dict['trigger']):
-                    #     for token in lista_tokens:
-                    #         if len(token)>1:
-                    #             if token in trigger:
-                    #                 codigo.append(ml_dict.at[i,'codigo'])
-                    # if len(codigo)>1:
-                    #     msg='No me queda claro qué quieres, aclárate!'
-                    #     for i in range(len(codigo)):
-                    #         msg+="\n"+str(i+1)+"    "+str(ml_dict[ml_dict['codigo']==codigo[i]]['nombre'].values[0])
-
-                    #     await message.channel.send(msg)
-                    #     with open('file_code', "wb") as file_code:
-                    #         pickle.dump(codigo, file_code)
-
-                    #     # Llama al cog que recibe la respuesta
-                    #     self.client.load_extension(f'cogs.cogElectorCodeAsk')
-                        
-                    # else:
-                    #     msg0='Aquí tienes el código, pedazo de crack \n\n'
-                    #     msg1=str(ml_dict[ml_dict['codigo']==codigo[0]]['nombre'].values[0])
-                    #     msg2='\n\n'+str(df_data[df_data['key']==codigo[0]]['value'].values[0])
-                    #     msg=msg0+msg1+msg2
-
-                    #     await message.channel.send(msg)
                     
                 # Request Bitcoin
                 elif tipo_request == "Bitcoin":
