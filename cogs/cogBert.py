d4be9da59cbbf80ec8ff87592dbed7528523675a
AdriCox <adrcoxgar@gmail.com>
2021-12-21 14:42:24 +0100

    funcionalizado la parte de "request"

--- cogs/cogBert.py
+++ cogs/cogBert.py
@@ -7,6 +7,7 @@ from funciones import modeloPuntuacion
 from funciones import modeloNBrequest
 from funciones import modeloMegat
 from funciones import funElectorCode
+from funciones import RequestFunction
 
 # Para gráficas
 import numpy as np
@@ -44,102 +45,19 @@ class CogBert(commands.Cog):
             # Intenciones:
             # 0: Info   1: Request  2: Ask  3: Fun
             await message.channel.send(str(intencion))
+
             # Modo Fun (3)
             if int(intencion)==3:
                 try:
                     await message.channel.send(modoFUN.funresponse(message,self))
                 except:
                     pass
+
             # Modo Request (1)
             elif int(intencion) == 1:
                 tipo_request=modeloNBrequest.decision_request(message.content)
-                await message.channel.send(str(tipo_request))
-                
-                # Request Elector Code
-                if tipo_request == "ML":
-                    await message.channel.send(str("TOMAS AQUI VA TU MAGIA"))
-                    client=self.client
-                    # df.at[str(message.channel),'cogBert']=0
-                    # with open('datos/pasarela_ch','wb') as fh:
-                    #     pickle.dump(df,fh)
-                    await funElectorCode.electorCode(lista_tokens,message,client)
-                    
-                # Request Bitcoin
-                elif tipo_request == "Bitcoin":
-                    await message.channel.send(str("Bitcoin"))
-                # Request Math
-                elif tipo_request == "math":
-                    a=0
-                    for i in message.content.split():
-                        try:
-                            await message.channel.send('Aquí tienes, no era tan difícil \n'+str(eval(i)))
-                            a=1
-                        except:
-                            pass
-                    if a==0:
-                        await message.channel.send(str("Mira, no tengo tiempo para esto. Mejor pregúntale a Daniela, que sabe mucho de estas cosas"))
-
-                # Request GridSearch
-                elif tipo_request == "grid":
-                    
-                    await message.channel.send(str("Dime qué optimizar"))
-                    #CogOptimo.preguntasIniciales(message)
-                    await message.channel.send(str("Dime qué optimizar y yo lo hago:")+str("Necesito un csv con los datos limpio, MUY LIMPIO \n"+"También que me especifiques qué modelo deseas \n")+"Pero vamos en orden, que nada de esto es mágico. ¿Qué tipo de modelo quieres?")
-                    df.at[str(message.channel),'cogBert']=0
-                    df.at[str(message.channel),'cogOptimo']=1
-                    with open('datos/pasarela_ch','wb') as fh:
-                        pickle.dump(df,fh)                    
-                    
-                    self.client.load_extension(f'cogs.cogOptimo')
-                elif tipo_request == "Install":
-                    #await message.channel.send(str("PIP lo que quieras"))
-
-                    # Buscador de instalador:
-                    import requests
-                    from bs4 import BeautifulSoup
-                    search="pytorch"
-                    for search in lista_tokens:
-                        try:
-                            url = "https://pypi.org/project/{}/".format(search)
-                            response = requests.get(url)
-                            soup = BeautifulSoup(response.text, "html.parser")
-                            pip = soup.find(class_ = "banner").find(class_="package-header__pip-instructions").find(id="pip-command").text
-                    
-                            await message.channel.send(pip)
-                        except:
-                            pass
-
-                elif tipo_request == "Grafica":
-                    #await message.channel.send(str("Mi gráfica es mejor"))
-
-                    try:
-                        y=message.content.split('=')[1]
-
-                        plt.rcParams["figure.figsize"] = [7.00, 3.50]
-                        # plt.rcParams["figure.autolayout"] = True
-                        x = np.arange(-10., 10., 0.01)
-                        #y = arctanh(x)
-                        fig = plt.figure()
-                        ax = fig.add_subplot(1, 1, 1)
-                        ax.spines['left'].set_position('center')
-                        ax.spines['bottom'].set_position('center')
-                        ax.spines['right'].set_color('none')
-                        ax.spines['top'].set_color('none')
-                        plt.plot(x, y, label='y='+str(y), c='blue',marker='.')
-                        plt.legend(loc=1)
-                        plt.savefig('datos/elmoline.png',dpi=150)#, bbox_inches='tight') # Guarda la imagen
-                        
-                        # Envía la imagen
-                        embed = discord.Embed(color=discord.Colour.blue())
-                        file = discord.File("datos/elmoline.png", filename="elmoline.png")
-                        embed.set_image(url="attachment://elmoline.png")
-                        await message.channel.send(file=file, embed=embed)
-                    except:
-                        
-                        await message.channel.send('Mejor preguntale a Daniela que sabe mucho de esto')
-
-                elif tipo_request == "Analisis":
-                    await message.channel.send(str("Te analizo lo que quieras"))
+                print('aqui viene la magia')
+                await message.channel.send(RequestFunction.decide_req(tipo_request))
 
             # Modo ask:
             elif int(intencion)==2:
---
+++ funciones/analRequest.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import pickle
+
+
+async def camerino(frase):
+    with open('columnas', 'rb') as files:
+        colum=pickle.load(files)
+    df = pd.DataFrame(columns = ['Lemma', 'Punct','Mayus', 'Ultima', 'Primera',
+                                 'Suf_sus', 'Suf_adj','Suf_ver','Suf_adv',
+                                 'Afi_dim', 'Afi_aum', 'Afi_des'])
+    frase = frase.split()
+    for i in range(len(frase)):
+        tupla = listalizador(frase,i)
+        df.at[i,:] = tupla
+    listas=[]
+    for i in range(len(df)):
+        conjunto=list(colum)
+        lista_metible=[0 for x in range(len(colum))]
+        if df.Lemma.iloc[i] in conjunto:
+            indice=conjunto.index(df.Lemma.iloc[i])
+            lista_metible[indice]=1
+        for co in df.columns:
+            if co in conjunto:
+                indice=conjunto.index(co)
+                lista_metible[indice]=df[co].iloc[i]
+        listas.append(lista_metible)
+    await listas
+
+
+async def listalizador(frase, indice):    
+
+#Sufijos. #Simplificandodummies    
+    palabra = frase[indice]
+    puntuacion = (',','"',"'",'.',';',':','(',')','[',']','-','<','>','!','¡','?','¿','—')
+    sustantivadores = ('ada', 'eda', 'ado', 'ero', 'aje', 'era', 'al', 'ismo', 'ar', 'ista',
+                   'ato','azgo', 'azo', 'ancia', 'ez', 'encia','eza', 'dad', 'ismo',
+                   'edad', 'ada', 'idad', 'itud', 'tad', 'or', 'ería', 'ada','era', 'ado',
+                   'ición', 'aje', 'sión','ante', 'ido', 'ente' ,'amento', 'iente' ,'amiento'
+                   'ción', 'imiento', 'ación','dor', 'ura')
+    adjetivadores = ('al', 'iento', 'ienta', 'ario', 'aria', 'il', 'ero', 'era', 'ista',
+                     'esco','esca','oso', 'osa', 'izo', 'iza','oide', 'able', 'ible',
+                     'ante', 'dor','ente', 'iente', 'ivo', 'iva', 'or', 'ano', 'ana', 'ío')
+    verbalizadores = ('ar', 'ear', 'ecer', 'izar', 'ar', 'ficar')
+    aumentativos = ('ón', 'ona', 'azo', 'aza', 'ote', 'ota', 'udo', 'uda')
+    diminutivos = ('ito', 'ita', 'ico', 'ica', 'illo', 'illa', 'ete', 'eta', 'ín', 'ina',
+              'ejo', 'eja', 'uelo', 'huela')
+    despectivo = ('aco', 'acho', 'acha', 'ajo', 'aja', 'ales', 'alla',
+              'ángano', 'ángana', 'ango', 'anga', 'astre', 'astro',
+              'astra', 'engue', 'ingo', 'ingue', 'orio', 'orrio',
+              'orro', 'orra', 'uco', 'uca', 'ucho', 'ucha', 'ujo',
+              'uja', 'ute', 'uza')       
+    lista = []        
+    lista.append(lemmatizer(palabra)) #LEMMA !!!!!!
+    lista.append(1 if palabra in puntuacion else 0) #es puntuacion
+    #lista.append(lemmatizer(frase[indice-1])if indice > 0 else ' ') #LEMMA PREVIO!!!!
+    #lista.append(lemmatizer(frase[indice+1])if indice<len(frase)-1 else ' ') #LEMMA POSTERIOR!!!!
+    lista.append(1 if palabra[0] == palabra[0].upper() else 0) #mayuscula?
+    lista.append(1 if indice == len(frase)-1 else 0) #es la última?
+    lista.append(1 if indice == 0 else 0) #es la primera?
+    
+    
+    if len(palabra)>3: 
+        #ciclo sustantivos
+        for i in range(2,8):
+            if palabra[-i:] in sustantivadores: 
+                lista.append(1)
+                break
+        if len(lista)<6:
+            lista.append(0)
+        #ciclo adjetivos
+        for i in range(2,6):
+            if palabra[-i:] in adjetivadores: 
+                lista.append(1)
+                break
+        if len(lista)<7:
+            lista.append(0)
+        #ciclo verbos
+        for i in range(2,6):
+            if palabra[-i:] in verbalizadores: 
+                lista.append(1)
+                break
+        if len(lista)<8:
+            lista.append(0)
+                       
+        lista.append(1 if palabra[-5:] == 'mente' else 0)
+       
+    
+        #ciclo diminutivos: generalmente sustantivos, también ADJ
+        for i in range(2,6):
+            if palabra[-i:] in diminutivos: 
+                lista.append(1)
+                break
+        if len(lista)<10:
+            lista.append(0)
+        #ciclo aumentativos: generalmente sustantivos, también ADJ
+        for i in range(2,4):
+            if palabra[-i:] in aumentativos: 
+                lista.append(1)
+                break
+        if len(lista)<11:
+            lista.append(0)
+        #ciclo despectivos: generalmente sustantivos, también ADJ
+        for i in range(3,7):
+            if palabra[-i:] in despectivo: 
+                lista.append(1)
+                break
+        if len(lista)<12:
+            lista.append(0)
+
+    else:
+        for i in range(7):
+            lista.append(0)
+        
+        
+    await tuple(lista)
+
+async def lemmatizer(palabra):
+    with open('rayo_lemmatizador' , 'rb') as f:
+        rl = pickle.load(f)
+    try:
+        await rl[rl['Form']==palabra].iloc[0,1]
+    except:
+        await 'No lemma'
+
+async def analizador(frase)
+	with open('columnas', 'rb') as files:
+	    colum=pickle.load(files)
+
+	dfiteo = pd.DataFrame(columns = colum)
+	for i,k in enumerate(camerino(frase)):
+	    dfiteo.loc[i] = k
+	with open('RF_all_model' , 'rb') as f:
+    	rf = pickle.load(f)
+    yhat = rf.predict(dfiteo)
+    await yhat
+
